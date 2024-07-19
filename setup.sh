23f1039200698da83dadb79d71607068db3f793c
Chris Balmer <chris@balmerfamilyfarm.com>
2024-07-19 15:33:13 -0400

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,2 +1,9 @@
+if [ ! -f "~/.zshrc" ]; then
+  mv ~/.zshrc ~/.zshrc.old
+fi
 ln -s .zshrc ~/.zshrc
+
+if [ ! -f "~/.config/starship.toml" ]; then
+  mv ~/.config/starship.toml ~/.config/starship.toml.old
+fi
 ln -s .config/starship.toml ~/.config/starship.toml
