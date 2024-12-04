8e8f7744d96f300a8d51d6de755a107cbab8e4b4
Chris Balmer <chris@labgophers.com>
2024-12-03 22:34:00 -0500

    added 1Password SSH setup

--- setup.sh
+++ setup.sh
@@ -9,3 +9,12 @@ if [ ! -f "~/.config/starship.toml" ]; then
   mv ~/.config/starship.toml ~/.config/starship.toml.old
 fi
 ln -s $(dirname "$0")/.config/starship.toml ~/.config/starship.toml
+
+# Setup 1Password SSH Agent on MacOS
+AGENT="$HOME/Library/Group Containers/2BUA8C4S2C.com.1password/t/agent.sock"
+NEW_AGENT="$HOME/.1password/agent.sock"
+mkdir -p ~/.1password
+
+if [[ -S $AGENT && ! -S $NEW_AGENT ]]; then
+    ln -s $AGENT $NEW_AGENT
+fi
